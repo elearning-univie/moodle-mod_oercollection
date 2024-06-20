35cb0220d6b1edc087c8c7a032aa08ab0079184f
Adrian Czermak <adrian.czermak@univie.ac.at>
2024-06-20 09:58:44 +0200

    devcommit

--- classes/plugininfo/oerapi.php
+++ classes/plugininfo/oerapi.php
@@ -38,4 +38,107 @@ class oerapi extends \core\plugininfo\base {
     {
         // TODO
     }
+
+    public static function plugintype_supports_disabling(): bool {
+        return true;
+    }
+
+    /**
+     * Finds all enabled plugins, the result may include missing plugins.
+     * @return array|null of enabled plugins $pluginname=>$pluginname, null means unknown
+     */
+    public static function get_enabled_plugins() {
+        global $DB;
+
+        $plugins = core_plugin_manager::instance()->get_installed_plugins('oerapi');
+        if (!$plugins) {
+            return array();
+        }
+        $installed = array();
+        foreach ($plugins as $plugin => $version) {
+            $installed[] = 'oerapi_'.$plugin;
+        }
+
+        list($installed, $params) = $DB->get_in_or_equal($installed, SQL_PARAMS_NAMED);
+        $disabled = $DB->get_records_select('config_plugins', "plugin $installed AND name = 'disabled'", $params, 'plugin ASC');
+        foreach ($disabled as $conf) {
+            if (empty($conf->value)) {
+                continue;
+            }
+            list($type, $name) = explode('_', $conf->plugin, 2);
+            unset($plugins[$name]);
+        }
+
+        $enabled = array();
+        foreach ($plugins as $plugin => $version) {
+            $enabled[$plugin] = $plugin;
+        }
+
+        return $enabled;
+    }
+
+    public static function enable_plugin(string $pluginname, int $enabled): bool {
+        $haschanged = false;
+
+        $plugin = 'oerapi_' . $pluginname;
+        $oldvalue = get_config($plugin, 'disabled');
+        $disabled = !$enabled;
+        // Only set value if there is no config setting or if the value is different from the previous one.
+        if ($oldvalue === false || ((bool) $oldvalue != $disabled)) {
+            set_config('disabled', $disabled, $plugin);
+            $haschanged = true;
+
+            add_to_config_log('disabled', $oldvalue, $disabled, $plugin);
+            \core_plugin_manager::reset_caches();
+        }
+
+        return $haschanged;
+    }
+
+    /**
+     * Return URL used for management of plugins of this type.
+     * @return moodle_url
+     */
+    public static function get_manage_url() {
+        return new moodle_url('/mod/assign/adminmanageplugins.php', array('subtype'=>'assignsubmission'));
+    }
+
+    public function get_settings_section_name() {
+        return $this->type . '_' . $this->name;
+    }
+
+    /**
+     * Loads plugin settings to the settings tree
+     *
+     * This function usually includes settings.php file in plugins folder.
+     * Alternatively it can create a link to some settings page (instance of admin_externalpage)
+     *
+     * @param \part_of_admin_tree $adminroot
+     * @param string $parentnodename
+     * @param bool $hassiteconfig whether the current user has moodle/site:config capability
+     */
+    public function load_settings(\part_of_admin_tree $adminroot, $parentnodename, $hassiteconfig) {
+        global $CFG, $USER, $DB, $OUTPUT, $PAGE; // In case settings.php wants to refer to them.
+        $ADMIN = $adminroot; // May be used in settings.php.
+        $plugininfo = $this; // Also can be used inside settings.php.
+
+        if (!$this->is_installed_and_upgraded()) {
+            return;
+        }
+
+        if (!$hassiteconfig or !file_exists($this->full_path('settings.php'))) {
+            return;
+        }
+
+        $section = $this->get_settings_section_name();
+
+        $settings = new \admin_settingpage($section, $this->displayname, 'moodle/site:config', $this->is_enabled() === false);
+
+        if ($adminroot->fulltree) {
+            $shortsubtype = substr($this->type, strlen('oerapi'));
+            include($this->full_path('settings.php'));
+        }
+
+        $adminroot->add($this->type . 'plugins', $settings);
+    }
 }
