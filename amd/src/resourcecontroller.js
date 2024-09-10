a3c6d1f2a75ccfee7abc3334e82fbe815e17a3f1
Adrian Czermak <adrian.czermak@univie.ac.at>
2024-09-10 12:24:06 +0000

    Resolve "Datenabfrage aus OERhub & Plugin-Cache"

--- amd/src/resourcecontroller.js
+++ amd/src/resourcecontroller.js
@@ -23,14 +23,14 @@ export const init = () => {
       }
     };
     $.mod_oercollection_set_visibility_oerentry = function(oer, oerentryid) {
-            ajax.call([{
-                methodname: 'mod_oercollection_set_visibility_oerentry',
-                args: {oerid: oer, oerentryid: oerentryid},
-                done: function () {
-                    location.reload();
-                },
-                fail: notification.exception
-            }]);
+        ajax.call([{
+            methodname: 'mod_oercollection_set_visibility_oerentry',
+            args: {oerid: oer, oerentryid: oerentryid},
+            done: function () {
+                location.reload();
+            },
+            fail: notification.exception
+        }]);
     };
     $.mod_oercollection_delete_oerentry = function(oer, oerentryid, resourcename) {
         getString('deletepopup', 'mod_oercollection', resourcename).then(function (warningmessage) {
@@ -55,90 +55,15 @@ export const init = () => {
             }
         });
     };
-    $.mod_oercollection_selected = function() {
-        var checkboxes = document.getElementsByName('selectbox');
-        var checkboxesChecked = [];
-        var listi = '';
-        for (var i=0; i<checkboxes.length; i++) {
-            if (checkboxes[i].checked) {
-                listi = listi + checkboxes[i].value + ' ';
-                checkboxesChecked.push(checkboxes[i]);
-            }
-        }
-    };
-    $.mod_oercollection_select_all = function(selected) {
-        $('input:checkbox').not(selected).prop('checked', selected.checked);
-        this.mod_oercollection_selected();
-    };
-    $.mod_oercollection_checkall = function() {
-        var checkboxes = document.getElementsByName('selectbox');
-        for (var i=0; i<checkboxes.length; i++) {
-            checkboxes[i].checked = true;
-        }
-    };
-    $.mod_oercollection_checknone = function() {
-        var checkboxes = document.getElementsByName('selectbox');
-        for (var i=0; i<checkboxes.length; i++) {
-            checkboxes[i].checked = false;
-        }
-    };
 
     $.mod_oercollection_move_resource_action = function(oer, oereidtomove, oereidmoveafter) {
-            ajax.call([{
-                methodname: 'mod_oercollection_move_resource',
-                args: {oerid: oer, oereidtomove: oereidtomove, oereidmoveafter: oereidmoveafter},
-                done: function () {
-                    location.reload();
-                },
-                fail: notification.exception
-            }]);
-    };
-
-    $.mod_oercollection_bulk_action = function(oer) {
-        var checkboxes = document.getElementsByName('selectbox');
-        var oerids = [];
-        var rlinks = [];
-        var todelete = 0;
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked == true) {
-                todelete++;
-                oerids[i] = checkboxes[i].value;
-                rlinks[i] = document.getElementById('resourcelink' + checkboxes[i].value).href;
-            }
-        }
-        var bulkaction = document.getElementById("editoptionselect").value;
-        var show = 0;
-        if (bulkaction == 1) {
-           for (var i = 0; i < rlinks.length; i++) {
-           window.open(rlinks[i], '_blank');
-           }
-        location.reload();
-        }
-        if (bulkaction == 2) {
-           show = 1;
-        }
-        if (bulkaction == 2 || bulkaction == 3) {
-                      ajax.call([{
-                methodname: 'mod_oercollection_set_visibility_all',
-                args: {oerid: oer, oerentryids: oerids, show: show},
-                done: function () {
-                    location.reload();
-                },
-                fail: notification.exception
-            }]);
-        }
-        if (bulkaction == 4) {
-                      ajax.call([{
-                methodname: 'mod_oercollection_delete_selected_oerentries',
-                args: {oerid: oer, oerentryids: oerids},
-                done: function () {
-                    var queryParams = new URLSearchParams(window.location.search);
-                    queryParams.set("delete", todelete);
-                    history.replaceState(null, null, "?"+queryParams.toString());
-                    location.reload();
-                },
-                fail: notification.exception
-            }]);
-        }
+        ajax.call([{
+            methodname: 'mod_oercollection_move_resource',
+            args: {oerid: oer, oereidtomove: oereidtomove, oereidmoveafter: oereidmoveafter},
+            done: function () {
+                location.reload();
+            },
+            fail: notification.exception
+        }]);
     };
 };
--- amd/src/searchcontroller.js
+++ amd/src/searchcontroller.js
@@ -3,27 +3,6 @@ import ajax from "core/ajax";
 import notification from "core/notification";
 
 export const init = () => {
-    $.mod_oercollection_selected = function() {
-        var checkboxes = document.getElementsByName('selectbox');
-        var checkboxesChecked = [];
-        var listi = '';
-        for (var i=0; i<checkboxes.length; i++) {
-            if (checkboxes[i].checked) {
-                listi = listi + checkboxes[i].value + ' ';
-                checkboxesChecked.push(checkboxes[i]);
-            }
-        }
-    };
-    $.mod_oercollection_select_all = function(selected) {
-        $('input:checkbox').not(selected).prop('checked', selected.checked);
-        this.mod_oercollection_selected();
-    };
-    $.mod_oercollection_checkall = function() {
-        var checkboxes = document.getElementsByName('selectbox');
-        for (var i=0; i<checkboxes.length; i++) {
-            checkboxes[i].checked = true;
-        }
-    };
     $.mod_oercollection_add_to_collection = function(oer, oerhubid, resourcelink, resourcename) {
         ajax.call([{
             methodname: 'mod_oercollection_add_to_collection',
@@ -33,45 +12,5 @@ export const init = () => {
             },
             fail: notification.exception
             }]);
-    };
- //   $.mod_oersearch_checknone = function() {
- //       var checkboxes = document.getElementsByName('selectbox');
- //       for (var i=0; i<checkboxes.length; i++) {
- //           checkboxes[i].checked = false;
- //       }
- //   };
-    $.mod_oercollection_bulk_action = function(oer) {
-      var checkboxes = document.getElementsByName('selectbox');
-        var oerids = [];
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked == true) {
-                oerids[i] = checkboxes[i].value;
-            }
-        }
-        var bulkaction = document.getElementById("editoptionselect").value;
-        var show = 0;
-        if (bulkaction == 2) {
-           show = 1;
-        }
-        if (bulkaction == 2 || bulkaction == 3) {
-                      ajax.call([{
-                methodname: 'mod_oercollection_set_visibility_all',
-                args: {oerid: oer, oerentryids: oerids, show: show},
-                done: function () {
-                    location.reload();
-                },
-                fail: notification.exception
-            }]);
-        }
-        if (bulkaction == 4) {
-                      ajax.call([{
-                methodname: 'mod_oercollection_delete_selected_oerentries',
-                args: {oerid: oer, oerentryids: oerids},
-                done: function () {
-                    location.reload();
-                },
-                fail: notification.exception
-            }]);
-        }
     };
 };
