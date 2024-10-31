a6155dc3d9356d09d4d3012bee696e8cabdc531b
Angela Baier <angela.baier@univie.ac.at>
2024-10-31 09:28:34 +0100

    updates

--- amd/src/searchcontroller.js
+++ amd/src/searchcontroller.js
@@ -8,13 +8,22 @@ export const init = () => {
         ajax.call([{
             methodname: 'mod_oercollection_add_to_collection',
             args: {oerid: oer, oerhubid: oerhubid, resourcelink: resourcelink, resourcename: resourcename},
-            done: () => {
-              getString('addedinfomessage', 'mod_oercollection', 1).then(function (infomessage) {
-              notification.addNotification({
-              message: infomessage,
-              type: "success"
-              });
-            });
+            done: function (returnval) {
+              if (returnval.alreadyincollection == 1) {
+                 getString('resourceexistsinfomessage', 'mod_oercollection').then(function (infomessage) {
+                 notification.addNotification({
+                 message: infomessage,
+                 type: "warning"
+                 });
+               });
+              } else {
+                 getString('addedinfomessage', 'mod_oercollection', 1).then(function (infomessage) {
+                 notification.addNotification({
+                 message: infomessage,
+                 type: "success"
+                 });
+               });
+             }
             },
             fail: notification.exception
             }]);
