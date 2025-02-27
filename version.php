9b7e417f610a5668ed10ff222a16b7fbd6a8899d
Angela Baier <angela.baier@univie.ac.at>
2025-02-27 11:15:18 +0100

    version

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_oercollection';
-$plugin->version = 2024100600.03;
+$plugin->version = 2024100600.04;
 $plugin->release = 'v4.5.0-r0';
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
