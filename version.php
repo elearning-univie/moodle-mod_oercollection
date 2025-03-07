ef2781114c81034201c0986e83d9ded357ca9667
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2025-03-07 11:50:56 +0000

    Update version.php

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_oercollection';
-$plugin->version = 2024100600.04;
-$plugin->release = 'v4.5.0-r0';
+$plugin->version = 2025030700.00;
+$plugin->release = 'v4.5-r0';
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
